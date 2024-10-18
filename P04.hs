c2b0a88ccc07e9ba49981dbd9fc86b486383b39a
Cynthia-dev-ux <cynthuau@ciencias.unam.mx>
2024-10-18 13:19:52 -0600

    Subir archivo

---
+++ P04.hs
@@ -0,0 +1,23 @@
+longitud :: Arbol a -> Int
+longitud = 
+
+profundidad :: Arbol a-> Int
+profundidad =
+
+ancho :: Arbol a-> Int
+ancho =
+
+recorrido :: Arbol a -> Recorrido -> [a]
+recorrido =
+
+niveles :: Arbol a -> [[a]]
+niveles =
+
+minimo :: ARbol a -> a
+minimo =
+
+maximo :: Arbol a -> a 
+maximo =
+
+eliminar :: Ord a => Arbol a -> a -> Arbol a
+eliminar = 
